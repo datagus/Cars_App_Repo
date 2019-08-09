5b4d8bc8a9a53f5675ed90d1647776bd067cf8ee
gusmairs <gusmairs@mac.com>
2019-08-09 15:41:34 -0700

    moved description to text file

--- server.R
+++ server.R
@@ -66,24 +66,41 @@ shinyServer(function(input, output) {
         print(g)
     })
 
-    str1 <- '<b>Shiny App Description</b>'
-    str2 <- 'The plot shows car make efficiency using number of cars
-             with MPG > 22 each year.'
-    str3 <- 'The plot dots are shaded for years when the global price of
-             oil exceeded $50 per barrel.'
-    str4 <- 'Use pull-down lists to choose another car maker.'
-    str5 <- 'Type or click to alter the range of years, from 1984 through 2016.'
-    str6 <- 'There are no input controls designed into the UI yet (e.g.
-             years prior to 1984).'
-    str7 <- 'The refresh button will clear up most errors.'
-    str8 <- ''
-    str9 <- '<a href=\'https://github.com/gusmairs/mpg-shiny-app.git\'>
-             GitHub Repo</a>'
+    # str1 <- '<b>Shiny App Description</b>'
+    # str2 <- 'The plot shows car make efficiency using number of cars
+    #          with MPG > 22 each year.'
+    # str3 <- 'The plot dots are shaded for years when the global price of
+    #          oil exceeded $50 per barrel.'
+    # str4 <- 'Use pull-down lists to choose another car maker.'
+    # str5 <- 'Type or click to alter the range of years, from 1984 through 2016.'
+    # str6 <- 'There are no input controls designed into the UI yet (e.g.
+    #          years prior to 1984).'
+    # str7 <- 'The refresh button will clear up most errors.'
+    # str8 <- ''
+    # str9 <- '<a href=\'https://github.com/gusmairs/mpg-shiny-app.git\'>
+    #          GitHub Repo</a>'
+
+    conn <- file('descrip.txt', open='r')
+    lines <- readLines(conn)
+    txt <- ''
+    for (line in lines){
+       txt <- paste(txt, line, '</br>')
+    }
 
     output$infoText <- renderUI({
-        HTML(paste(
-            str1, str2, str3, str4, str5, str6, str7, str8, str9,
-            sep = '<br/>'
-        ))
+        HTML(txt)
+        # HTML(paste(
+            # str1, str2, str3, str4, str5, str6, str7, str8, str9,
+            # sep = '<br/>'
+        # ))
     })
 })
+
+# conn <- file('descrip.txt', open='r')
+# lines <- readLines(conn)
+# txt <- ''
+# for (line in lines){
+#    txt <- paste(txt, line, '</br>')
+# }
+# print(HTML(txt))
+# close(conn)
--- ui.R
+++ ui.R
@@ -1,45 +1,42 @@
 library(ggplot2)
 library(shiny)
 
-cardat <- read.csv("cardat.csv")
+cardat <- read.csv('cardat.csv')
 makes <- sort(unique(cardat$make))
 
-
 shinyUI(fluidPage(
-    titlePanel("Car Makes Efficiency Plot"),
+    titlePanel('Car Makes Efficiency Plot'),
     sidebarLayout(
         sidebarPanel(
-            selectInput(inputId = "make1", 
-                        label = strong("Car Make 1"), 
-                        choices = makes, 
-                        selected = "Ford"
-            ), 
-            selectInput(inputId = "make2", 
-                        label = strong("Car Make 2"), 
-                        choices = makes, 
-                        selected = "Honda"
-            ), 
-            numericInput(inputId = "start",
-                         label = "Starting year: ",
+            selectInput(inputId = 'make1',
+                        label = strong('Car Make 1'),
+                        choices = makes,
+                        selected = 'Ford'
+            ),
+            selectInput(inputId = 'make2',
+                        label = strong('Car Make 2'),
+                        choices = makes,
+                        selected = 'Honda'
+            ),
+            numericInput(inputId = 'start',
+                         label = 'Starting year: ',
                          value = 2001,
                          min = min(cardat$year),
                          max = max(cardat$year)
             ),
-            numericInput(inputId = "span",
-                         label = "Years to show: ",
+            numericInput(inputId = 'span',
+                         label = 'Years to show: ',
                          value = 15,
                          min = 1,
                          max = max(cardat$year) - min(cardat$year) + 1
             )
         ),
         mainPanel(
-            tabsetPanel(type = "tabs",
-                        tabPanel("Plot", br(), plotOutput("sctPlot")),
-                        tabPanel("Info", br(), htmlOutput("infoText"))
+            tabsetPanel(type = 'tabs',
+                        tabPanel('Plot', br(), plotOutput('sctPlot')),
+                        tabPanel('Info', br(), htmlOutput('infoText'))
                         ),
-            verbatimTextOutput("carN")
+            verbatimTextOutput('carN')
         )
     )
 ))
-
-
